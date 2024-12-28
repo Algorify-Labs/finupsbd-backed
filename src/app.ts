b096523b16cc6c66c212b55132f38ad5899bfcd7
Shamimreza82 <shamimrezaone@gmail.com>
2024-12-28 23:22:01 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+app.use(express.json());
+
+
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({ message: 'Server is running' });
+});
+
+
+
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import app from "./app";
+
+const main = async () => {
+    try {
+        
+        app.listen()
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main()
